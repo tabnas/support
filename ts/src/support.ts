f3f07cb59ea1ed26c96463e232f6359975026edf
Richard Rodger <richard@ricebridge.com>
2026-08-11 13:21:49 +0100

    v0.1.1

--- ts/src/support.ts
+++ ts/src/support.ts
@@ -22,7 +22,7 @@
  *     .file(Path.join(findSpecDir(__dirname), 'happy.tsv'))
  */
 
-export const VERSION = '0.1.0'
+export const VERSION = '0.1.1'
 
 export { unescape, escape } from './escape'
 
